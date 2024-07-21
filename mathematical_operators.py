c6ec3b0773d48c3b63cb51aab181a8942e3e7651
Haider Rasool Qadri <haiderqadri.07@gmail.com>
2024-07-21 10:28:39 +0500

    Mathematical Operators (+, -, *,  /,  //, %)

---
+++ mathematical_operators.py
@@ -0,0 +1,19 @@
+# Let's start 
+
+# Addition
+print(13 + 3)
+
+# Subtraction
+print(13 - 3)
+
+# Multiplication
+print(13 * 3)
+
+# Division (Answer containing float number)
+print(13 / 3)
+
+# Divison (Print only interger)
+print(13 // 3)
+
+# Remainder after division
+print(13 % 3)
